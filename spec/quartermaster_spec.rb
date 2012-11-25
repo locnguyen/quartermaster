7411352a40ac15396edcf7dfffd1b10958a0d332
Loc Nguyen <lochnguyen@gmail.com>
2012-11-25 09:16:47 -0800

    Extracted Product API specs into its own file for better organization

--- spec/quartermaster_spec.rb
+++ spec/quartermaster_spec.rb
@@ -1,6 +1,5 @@
 require_relative 'spec_helper'
 require 'sinatra'
-require 'ostruct'
 
 describe "The Quartermaster API", :type => :api do
 
@@ -12,96 +11,5 @@ describe "The Quartermaster API", :type => :api do
   it "should respond with OK at the root" do
     get '/'
     last_response.should be_ok
-  end
-
-  describe "/products" do
-
-    context "GET" do
-      it "should respond with all products" do
-        50.of { Product.gen }
-
-        get '/products'
-        results = JSON.parse(last_response.body)
-        results.length.should == 50
-      end
-
-      it "should respond with products matching manufacturer" do
-        10.of { Product.gen(:panasonic) }
-
-        get '/products?manufacturer=Panasonic'
-        results = JSON.parse(last_response.body)
-        results.length.should == 10
-      end
-
-      it "should respond with products matching model name" do
-        Product.gen(:panasonic_gh3)
-
-        get '/products?model_name=GH3'
-        LOG.debug last_response.body
-        results = JSON.parse(last_response.body)
-        results[0]['model_name'].should be == 'GH3'
-      end
-    end
-
-    context "POST" do
-      subject { Product.make }
-
-      it "should respond with 201 if successful" do
-        post '/products', subject.to_json
-        last_response.status.should be == 201
-      end
-
-      it "should respond with an ID" do
-        post '/products', subject.to_json
-        data = OpenStruct.new(JSON.parse(last_response.body))
-        data.id.should be
-      end
-
-      it "should respond with a date for created_at" do
-        post '/products', subject.to_json
-        json = JSON.parse(last_response.body)
-        json['created_at'].should be
-      end
-    end
-  end
-
-  describe "/product/:id" do
-
-    subject { Product.gen }
-
-    context "GET" do
-      it "should respond with a product with the ID" do
-        get "/product/#{subject.id}"
-        data = OpenStruct.new(JSON.parse(last_response.body))
-        data.id.should be
-      end
-
-      it "should respond with a 404 when the product doesn't exist" do
-        get '/product/999'
-        last_response.status.should be == 404
-      end
-    end
-
-    context "PUT" do
-      it "should respond with 200 if successful" do
-        before_manufacturer = subject.manufacturer
-        subject.manufacturer = before_manufacturer + rand(3).to_s
-        put "/product/#{subject.id}", subject.to_json
-        last_response.status.should be == 200
-      end
-
-      it "should respond with the updated product" do
-        before_manufacturer = subject.manufacturer
-        subject.manufacturer = before_manufacturer + rand(3).to_s
-        put "/product/#{subject.id}", subject.to_json
-        data = OpenStruct.new(JSON.parse(last_response.body))
-        data.manufacturer.should be == before_manufacturer
-      end
-
-      it "should respond with a 404 when the product doesn't exist" do
-        put '/product/9999', subject.to_json
-        last_response.status.should be == 404
-      end
-    end
   end
 end
