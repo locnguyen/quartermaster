b6c6b2eb52446a461fd3698afd49c3cb16ef23f7
Loc Nguyen <lochnguyen@gmail.com>
2012-11-26 16:18:05 -0800

    Fixed problem with Asset fixture not saving by requiring the Time class so we can convert the random value to a date object

--- spec/spec_fixtures.rb
+++ spec/spec_fixtures.rb
@@ -1,4 +1,5 @@
 require 'dm-sweatshop'
+require 'time'
 
 include DataMapper::Sweatshop::Unique
 
@@ -20,6 +21,6 @@ Product.fixture(:panasonic_gh3) {{
 
 Asset.fixture {{
   :serial_number => unique(:serial_number) { /\w+/.gen },
-  :service_tag => unique(:service_tag) { /\w+/.gen }
-  #:acquire_date => Time.at(rand * Time.now.to_f)
+  :service_tag => unique(:service_tag) { /\w+/.gen },
+  :acquire_date => Time.at(rand * Time.now.to_i).to_date
 }}
